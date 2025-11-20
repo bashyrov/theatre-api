f4acc64f86283683fa71e28210963abcc4ed0f30
Artur Bashyrov <bashyroov@gmail.com>
2025-11-20 03:46:54 +0100

    initialized TheatreHall model

--- theatre_app/models.py
+++ theatre_app/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_per_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def total_seats(self):
+        return self.rows * self.seats_per_row
+
