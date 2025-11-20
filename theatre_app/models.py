a3e1b1a860bec18540b530108ba537cca4a647c3
Artur Bashyrov <bashyroov@gmail.com>
2025-11-20 03:52:55 +0100

    Initilized Performance model

--- theatre_app/models.py
+++ theatre_app/models.py
@@ -25,3 +25,14 @@ class Actor(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+class Performance(models.Model):
+    play = models.CharField(max_length=200) #TODO: Change to ForeignKey to Play model when created
+    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.play} at {self.show_time} in {self.theatre_hall.name}"
+
+
+
+
